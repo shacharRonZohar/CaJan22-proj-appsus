99315777c220e825a66ce3bb8ede5a389dc5f339
Adam <adampn462@gmail.com>
2022-03-02 12:51:51 +0200

    edit noteService and keepApp cmp

--- js/pages/keep-app.cmp.js
+++ js/pages/keep-app.cmp.js
@@ -1,26 +1,32 @@
+import { noteService } from '../apps/keep/services/noteService.js'
+import noteList from '../apps/keep/cmps/note-list.cmp.js'
+
 export default {
-    props: [''],
-    emits: [''],
     template: `
-        <section>This is a keep page!</section>
+        <section class="keep-app">
+            This is a keep page!<br>
+            // note-filter <br>
+            // note-list // note-preview
+            <note-list :notes></note-list>
+            <pre>{{notes}}</pre>
+        </section>
     `,
     components: {
-
+        noteList
     },
     data() {
         return {
-
+            notes: null
         }
     },
     created() {
-
+        this.updateNotes()
     },
-    unmounted() {
-
-    },
-
     methods: {
-
+        updateNotes() {
+            noteService.query()
+                .then(notes => this.notes = notes)
+        }
     },
     computed: {
 
