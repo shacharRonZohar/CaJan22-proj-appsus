4a9ee72eab3ffd9f9b03cc4a7a917b0dd6e8969f
Adam <adampn462@gmail.com>
2022-03-03 20:57:23 +0200

    some more style

--- js/apps/keep/cmps/note-add.cmp.js
+++ js/apps/keep/cmps/note-add.cmp.js
@@ -6,14 +6,16 @@ export default {
     <section class="note-add">
         <div class="take-note">
             <form>
-                <input v-model="newNote.info.title" type="text" placeholder="Title">
-                <!-- <div v-model="newNote.info.title" contenteditable="true"></div> -->
-                <input v-model="newNote.info.txt" type="text" placeholder="Take a note...">
-                <button class="add-btn btn icon" @click.prevent="addNote"></button>
-                <button class="txt-btn btn icon" @click.prevent="setTxtType"></button>
-                <button class="img-btn btn icon" @click.prevent="setImgType"></button>
-                <button class="vid-btn btn icon" @click.prevent="setVidType"></button>
-                <pre>{{newNote}}</pre>
+                <div>
+                    <input v-model="newNote.info.title" type="text" placeholder="Title">
+                    <input v-model="newNote.info.txt" type="text" placeholder="Take a note...">
+                    <button class="add-btn btn icon" @click.prevent="addNote" title="Add Note"></button>
+                    <button class="txt-btn btn icon" @click.prevent="setTxtType" title="Text Note"></button>
+                    <button class="img-btn btn icon" @click.prevent="setImgType" title="Image Note"></button>
+                    <button class="vid-btn btn icon" @click.prevent="setVidType" title="Video Note"></button>
+                    <!-- <pre>{{newNote}}</pre> -->
+                </div>
+
             </form>
         </div>
     </section>
@@ -27,8 +29,6 @@ export default {
         addNote() {
             noteService.addNote(this.newNote)
                 .then((note) => this.$emit('noteAdded', note));
-            
-            // this.newNote = noteService.getEmptyNote('note-txt')
         },
         setTxtType() {
             this.newNote = noteService.getEmptyNote('note-txt')
