7281eaa7c25606061a8a3aac0171a45341f86378
RZShachar <RZShachar@gmail.com>
2022-03-04 12:46:27 +0200

    Added fake search bar

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -5,7 +5,8 @@ export default {
     emits: [''],
     template: `
         <section class="email-preview" :class="isRead">
-            <div class="icon"></div>
+            <input type="checkbox" class="star icon" />
+            <div class="star icon"></div>
             <span class="name-txt">{{email.from.fullName}}</span>
             <div class="txt-container">
                 <span class="subject">{{subjectTxt}}</span>
